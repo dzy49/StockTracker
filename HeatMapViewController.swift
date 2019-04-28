cae3f41a1ad26d5cd862ad058bcfa3071bdc8bf3
Mike <deng199849@Gmail.com>
2019-04-28 04:36:22 -0400

    heat map mostly done

--- HeatMapViewController.swift
+++ HeatMapViewController.swift
@@ -17,8 +17,9 @@ class IndexCell: UITableViewCell{
 }
 class HeatMapViewController: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITableViewDataSource {
     
-    
-    var currCountry:String="none"
+    @IBOutlet weak var MarketName: UILabel!
+    var spinner:UIActivityIndicatorView=UIActivityIndicatorView()
+    var currIndex=[String]()
     @IBOutlet weak var WorldMapView: UIScrollView!
     @IBOutlet weak var MarketTableView: UITableView!
     let map: FSInteractiveMapView = FSInteractiveMapView()
@@ -34,21 +35,28 @@ class HeatMapViewController: UIViewController,UIScrollViewDelegate,UITableViewDe
         return marketCount
     }
     
-    
-    func getMarketData(marketName:String)->(String,String){
-        return ("1","1000")
+
+    func getMarketData(marketName:String)->(Double,String){
+       
+        var price=0.0
+        if let p=(model.heatmapdict[marketName]?.price){
+            price=p
+        }
+        return (price,"1000")
         //TODO
     }
     
+   
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = MarketTableView.dequeueReusableCell(withIdentifier: "IndexCell", for: indexPath)
-        let price=getMarketData(marketName: currCountry).0
-        let volume=getMarketData(marketName: currCountry).0
+        let price=getMarketData(marketName: currIndex[indexPath.row]).0
+        let volume=getMarketData(marketName: currIndex[indexPath.row]).1
         if let myCell =  cell as? IndexCell {
-            myCell.name.text = currCountry
-            myCell.price.text = price
+            myCell.name.text = currIndex[indexPath.row]
+            myCell.price.text = String(price)
             myCell.volume.text = volume
-            if(Int(price)!>0){
+            if(price>0){
                 myCell.price.backgroundColor = UIColor(red: 0, green: 0.7556203008, blue: 0.1655870676, alpha: 1)
             }else{
                 myCell.price.backgroundColor=UIColor.red
@@ -61,14 +69,30 @@ class HeatMapViewController: UIViewController,UIScrollViewDelegate,UITableViewDe
     func computeColors()->[String:UIColor]{
         //TODO
         var mapColor = [String: UIColor]()
+        if let p=model.heatmapdict["DJI"]?.price{
+            if(p > 0.0){
+                mapColor["US"]=UIColor.green
+            }else{
+                mapColor["US"]=UIColor.red
+            }
+        }
+        
+        if let p=model.heatmapdict["000001.SHH"]?.price{
+            if(p > 0.0){
+                mapColor["CN"]=UIColor.green
+            }else{
+                mapColor["CN"]=UIColor.red
+            }
+        }
         return mapColor
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        WorldMapView.delegate=self
-        MarketTableView.delegate=self
-        MarketTableView.dataSource=self
+    override func viewDidAppear(_ animated: Bool) {
+        if(model.loaded==false){
+            model.LoadHeatMapData()
+           // model.getSesssionDataTask()
+            spinner.stopAnimating()
+        
         weak var oldClickedLayer = CAShapeLayer()
         
         var mapData              = [String: Int]()
@@ -80,54 +104,73 @@ class HeatMapViewController: UIViewController,UIScrollViewDelegate,UITableViewDe
         mapData["europe"]        = 20
         
         
-        var mapData2            = [String: UIColor]()
-        mapData2["US"]          = UIColor.red
-     
+        //var mapData2            = [String: UIColor]()
+        //mapData2["US"]          = UIColor.red
+        
         var mapColors = [UIColor]()
         mapColors.append(UIColor.lightGray)
         mapColors.append(UIColor.darkGray)
         map.frame = WorldMapView.frame
         map.clickHandler = {(identifier: String? , _ layer: CAShapeLayer?) -> Void in
             if(identifier=="CN"||identifier=="US"){
-            if (oldClickedLayer != nil) {
-                oldClickedLayer?.zPosition = 0
-                oldClickedLayer?.shadowOpacity = 0
+                if (oldClickedLayer != nil) {
+                    oldClickedLayer?.zPosition = 0
+                    oldClickedLayer?.shadowOpacity = 0
+                    
+                }
+                oldClickedLayer = layer
+                // We set a simple effect on the layer clicked to highlight it
+                layer?.zPosition = 10
+                layer?.shadowOpacity = 0.5
+                layer?.shadowColor = UIColor.black.cgColor
+                layer?.shadowRadius = 5
+                layer?.shadowOffset = CGSize(width: 0, height: 0)
+                //layer?.shadowColor = UIColor.blue.cgColor;
+                //layer?.fillColor=UIColor.blue.cgColor;
+                //print(identifier)
+                if(identifier=="CN"){
+                    self.MarketName.text="China"
+                    self.currIndex=model.marketIndex["CN"]!
+                    self.marketCount=1
+                    self.MarketTableView.reloadData()
+                }else if(identifier=="US"){
+                    self.MarketName.text="United States"
+                    self.currIndex=model.marketIndex["US"]!
+                    self.marketCount=2
+                    self.MarketTableView.reloadData()
+                    
+                }
                 
             }
-            oldClickedLayer = layer
-            // We set a simple effect on the layer clicked to highlight it
-            layer?.zPosition = 10
-            layer?.shadowOpacity = 0.5
-            layer?.shadowColor = UIColor.black.cgColor
-            layer?.shadowRadius = 5
-            layer?.shadowOffset = CGSize(width: 0, height: 0)
-            //layer?.shadowColor = UIColor.blue.cgColor;
-            //layer?.fillColor=UIColor.blue.cgColor;
-            //print(identifier)
-            if(identifier=="CN"){
-                self.currCountry="CN"
-                self.marketCount=1
-                self.MarketTableView.reloadData()
-            }else if(identifier=="US"){
-                self.currCountry="US"
-                self.marketCount=1
-                self.MarketTableView.reloadData()
-            }
-                
-            model.getSesssionDataTask()
-            }
+            
         }
         
         let mapName: String! = String("world-low")
         //map.color
         //map.loadMap(mapName, withData:mapData, colorAxis:mapColors)
-        map.loadMap(mapName, withColors: mapData2)
+        map.loadMap(mapName, withColors: computeColors())
         WorldMapView.addSubview(map)
         WorldMapView.setNeedsDisplay()
         WorldMapView.contentSize=CGSize(width: 1000, height: 1000)
-            //WorldMapView.frame.size
+        //WorldMapView.frame.size
         WorldMapView.maximumZoomScale=4
         WorldMapView.minimumZoomScale=0.5
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        WorldMapView.delegate=self
+        MarketTableView.delegate=self
+        MarketTableView.dataSource=self
+//        model.LoadHeatMapData()
+        //model.getSesssionDataTask()
+        spinner.center=view.center
+        spinner.style=UIActivityIndicatorView.Style.gray
+        view.addSubview(spinner)
+        spinner.startAnimating()
+        spinner.hidesWhenStopped=true
+        
         
     }
     
--- model.swift
+++ model.swift
@@ -7,28 +7,53 @@
 //
 
 import Foundation
-struct Beer {
-   
-}
+
 class model{
-    static func getSesssionDataTask()  {
+    static var isLoading : Bool = false
+    static var failed=false
+    static var loaded=false
+    static var price = 0.0
+    static var heatmapdict:[String:(price:Double,change:Double,volume:Double)] =
+    [String:(price:Double,change:Double,volume:Double)]()
+    static var marketIndex:[String:[String]]=["US":["DJI","NASDAQ:^IXIC"],"CN":["000001.SHH"]]
+    static func LoadHeatMapData(){
+        getStockJson(symbol: "DJI")
+        getStockJson(symbol: "NASDAQ:^IXIC")
+        getStockJson(symbol: "000001.SHH")
+    }
+    static func getSesssionDataTask(){
+        let semaphore = DispatchSemaphore(value: 0)
+        //->Double  {
+        isLoading=true
+        var dateprice=[String:String]()
         let urlStr="https://www.alphavantage.co/query?function=TIME_SERIES_Daily&symbol=DJI&apikey=6C2PR5ZBG60H6490"
         let sess=URLSession.shared
         let urls:NSURL=NSURL.init(string: urlStr)!
         let request:URLRequest=NSURLRequest.init(url: urls as URL) as URLRequest
+        let group = DispatchGroup()
+        group.enter()
         let task = sess.dataTask(with: request){(data, res, error) in
+            
             if(error==nil){
                 do{
                     let dict = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-                    print(dict)
+                    //print(dict)
                     if let time = dict["Time Series (Daily)"] as? NSDictionary  {
                         for (key, value) in time {
                             if let value = value as? Dictionary<String, String> {
                                 if let close = value["4. close"] {
-                                    print("\(key) CloseStock-> \(close)")
+                                    let i = key as! String
+                                    dateprice[i]=close
+                                    //("\(key) CloseStock-> \(close)")
+                                    //price = callback(dateprice: dateprice)
                                 }
                             }
                         }
+                        
+                       // price=callback(dateprice: dateprice)
+                        semaphore.signal()
+                        group.leave()
+
                     }
                 }catch{
                     print(error.localizedDescription)
@@ -37,6 +62,112 @@ class model{
             }
         }
         task.resume()
-        print("StockTracker")
+        //_ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        let timeout = DispatchTime.now() + .seconds(5)
+        
+        if semaphore.wait(timeout: timeout) == .timedOut {
+            failed=true
+            print("failed")
+        }else{
+            callback(marketName: "DJI",dateprice: dateprice)
+        }
+        //TODO
+        //print("p:"+String(price))
+       
+    }
+    
+    static func getStockJson(symbol:String){
+        let semaphore = DispatchSemaphore(value: 0)
+        var baseUrl="https://www.alphavantage.co/query?function=TIME_SERIES_Daily&symbol="
+        let apiKey="&apikey=6C2PR5ZBG60H6490"
+        var dateprice=[String:String]()
+        
+        
+        let url = baseUrl+symbol+apiKey
+        let urlStr : NSString = url.addingPercentEncoding(withAllowedCharacters:.urlQueryAllowed) as! NSString
+       // let searchURL : NSURL = NSURL(string: urlStr as String)!
+        //print(searchURL)
+        
+        
+        let sess=URLSession.shared
+        let urls:NSURL=NSURL.init(string: urlStr as String)!
+        let request:URLRequest=NSURLRequest.init(url: urls as URL) as URLRequest
+        let group = DispatchGroup()
+        group.enter()
+        let task = sess.dataTask(with: request){(data, res, error) in
+            
+            if(error==nil){
+                do{
+                    let dict = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    //print(dict)
+                    if let time = dict["Time Series (Daily)"] as? NSDictionary  {
+                        for (key, value) in time {
+                            if let value = value as? Dictionary<String, String> {
+                                if let close = value["4. close"] {
+                                    let i = key as! String
+                                    dateprice[i]=close
+                                    //("\(key) CloseStock-> \(close)")
+                                    //price = callback(dateprice: dateprice)
+                                }
+                            }
+                        }
+                        
+                        // price=callback(dateprice: dateprice)
+                        semaphore.signal()
+                        group.leave()
+                        
+                    }
+                }catch{
+                    print(error.localizedDescription)
+                    
+                }
+            }
+        }
+        task.resume()
+        //var price=0.0
+        
+        let timeout = DispatchTime.now() + .seconds(5)
+        
+        if semaphore.wait(timeout: timeout) == .timedOut {
+            failed=true
+            print("failed")
+        }else{
+            callback(marketName:symbol,dateprice: dateprice)
+        }
+        
+        //TODO
+        //print("p:"+String(price))
+        
+    }
+   
+    static func callback(marketName:String,dateprice:[String:String]){
+        //->Double{
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd"
+        
+        let sortedArray = dateprice
+            //First map to an array tuples: [(Date, [String:Int]]
+            .map{(df.date(from: $0.key)!, [$0.key:$0.value])}
+            
+            //Now sort by the dates, using `<` since dates are Comparable.
+            .sorted{$0.0 < $1.0}
+            
+            //And re-map to discard the Date objects
+            //.map{$1}
+        //print(sortedArray)
+    let lastdayprice=sortedArray[sortedArray.count-1].1.first?.value
+    let lastwodayprice=sortedArray[sortedArray.count-2].1.first?.value
+    let change=Double(lastdayprice!)!-Double(lastwodayprice!)!
+        let percentage=change/Double(lastdayprice!)!*100
+    print("cb:"+String(change))
+    isLoading=false
+        if(marketName=="NASDAQ:^IXIC"){
+            loaded=true
+        }
+    price=change
+    heatmapdict[marketName]=(change,percentage,0.0)
+    print(heatmapdict)
+   // var change=Double(sortedArray[sortedArray.count].1.values.lazy())-Double(sortedArray[sortedArray.count-1].1.values[0])
+        //return sortedArrayOfDicts.last
     }
 }
