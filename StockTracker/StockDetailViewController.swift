082dde73cffa57421b545e9c33d3c764e65fb312
Mike <deng199849@Gmail.com>
2019-05-11 08:49:27 -0400

    update

--- StockTracker/StockDetailViewController.swift
+++ StockTracker/StockDetailViewController.swift
@@ -46,11 +46,72 @@ class StockDetailViewController:UIViewController,ChartDelegate{
     //@IBOutlet weak var labelLeadingMarginConstraint: NSLayoutConstraint!
     @IBOutlet weak var label: UILabel!
    // fileprivate var labelLeadingMarginInitialConstant: CGFloat!
+    var newView:UIView=UIView(frame: CGRect(x: 1, y:1, width: 1, height: 1))
+    var button=UIButton(frame: CGRect(x: 1, y:1, width: 1, height: 1))
+    var blackView=UIView(frame: CGRect(x: 1, y:1, width: 1, height: 1))
+    var settingOpen=false
+    @IBAction func SettingButton(_ sender: UIButton) {
+        newView.isHidden = !newView.isHidden
+        blackView.isHidden = !blackView.isHidden
+
+    }
+    func updateView(){
+        let windowSize=CGSize(width: self.view.frame.width*0.7, height: self.view.frame.height/2)
+        let windowPoint=CGPoint(x: self.view.frame.maxX*0.15, y: self.view.frame.maxY*0.2)
+        let windowRect=CGRect(origin: windowPoint, size: windowSize)
+        newView.frame=windowRect
+        let buttonRect=CGRect(origin: CGPoint(x: newView.frame.minX*0.7, y:newView.frame.minY) , size: CGSize(width: newView.frame.width*0.7, height: newView.frame.height/2))
+        button.frame=buttonRect
+        
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    override func viewDidLayoutSubviews() {
+        updateView()
+        super.viewDidLayoutSubviews()
+        
+    }
+    @objc func buttonAction(sender: UIButton!) {
+        let btnsendtag: UIButton = sender
+        if btnsendtag.tag == 1 {
+            newView.isHidden = !newView.isHidden
+            blackView.isHidden = !blackView.isHidden
+        }
+        print("?????")
+    }
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        newView.isHidden = !newView.isHidden
+        blackView.isHidden = !blackView.isHidden
+    }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        let windowSize=CGSize(width: self.view.frame.width*0.7, height: self.view.frame.height/2)
+        let windowPoint=CGPoint(x: self.view.frame.maxX*0.15, y: self.view.frame.maxY*0.2)
+        let windowRect=CGRect(origin: windowPoint, size: windowSize)
+        newView=UIView(frame: windowRect)
+        newView.backgroundColor=UIColor.white
+        blackView=UIView(frame:self.view.frame)
+        blackView.backgroundColor=UIColor.black
+        blackView.alpha=0.35
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        blackView.addGestureRecognizer(tap)
+        UIApplication.shared.keyWindow?.addSubview(blackView)
+
+        UIApplication.shared.keyWindow?.addSubview(newView)
+
+        button=UIButton(frame: CGRect(origin: CGPoint(x: newView.frame.minX, y:newView.frame.minY) , size: CGSize(width: newView.frame.width*0.7, height: newView.frame.height/2)))
+        button.titleLabel?.text="??"
+        button.tag=1
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.backgroundColor=UIColor.red
+        newView.layer.cornerRadius=6
+        newView.addSubview(button)
+        newView.isHidden=true
+        blackView.isHidden=true
         StockChart.delegate=self
         //labelLeadingMarginInitialConstant = labelLeadingMarginConstraint.constant
         var followbutton=UIBarButtonItem(title:"+ Follow", style: .plain, target: self, action: nil)
